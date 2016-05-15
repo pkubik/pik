352cb3dbd5db5afbd6f74456320af266ca3cf402
pkubik <pkubik>
2016-05-15 21:36:56 +0200

    Actuator to show application metadata/health. Database connection limit <- 3.

--- src/main/java/pl/edu/pw/elka/pik/PikApplication.java
+++ src/main/java/pl/edu/pw/elka/pik/PikApplication.java
@@ -11,13 +11,6 @@ public class PikApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(PikApplication.class, args);
-		System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-		String[] beanNames = ctx.getBeanDefinitionNames();
-		Arrays.sort(beanNames);
-		for (String beanName : beanNames) {
-			System.out.println(beanName);
-		}
 	}
 
 }
