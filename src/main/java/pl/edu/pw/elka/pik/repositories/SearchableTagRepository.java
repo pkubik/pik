e749780bb5bc3dec82e45539b8d722d4d988af0d
Pawel Kubik <p.kubik.pl@gmail.com>
2016-06-05 20:01:54 +0200

    Implement full-text-search view

--- src/main/java/pl/edu/pw/elka/pik/repositories/SearchableTagRepository.java
+++ src/main/java/pl/edu/pw/elka/pik/repositories/SearchableTagRepository.java
@@ -1,11 +1,10 @@
 package pl.edu.pw.elka.pik.repositories;
 
-import org.springframework.data.jpa.repository.Query;
 import pl.edu.pw.elka.pik.entities.Tag;
 
 /**
  * @author pkubik.
  */
 public interface SearchableTagRepository {
-
+    Iterable<Tag> searchTags(String keywords);
 }
--- src/main/java/pl/edu/pw/elka/pik/repositories/TagRepositoryImpl.java
+++ src/main/java/pl/edu/pw/elka/pik/repositories/TagRepositoryImpl.java
@@ -2,11 +2,31 @@ package pl.edu.pw.elka.pik.repositories;
 
 import pl.edu.pw.elka.pik.entities.Tag;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author pkubik.
  */
 public class TagRepositoryImpl implements SearchableTagRepository {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
     public Iterable<Tag> searchTags(String keywords) {
-        return null;
+        Query query = entityManager.createNativeQuery("SELECT search.id, search.name, search.description\n" +
+                "FROM\n" +
+                "  (SELECT id, name, description,\n" +
+                "   ts_rank(text_search_vector, to_tsquery(?1)) as relevancy\n" +
+                "   FROM tag) AS search\n" +
+                "WHERE relevancy > 0\n" +
+                "ORDER BY relevancy DESC;", Tag.class);
+
+        String transformed = keywords.replaceAll(" ", " | ");
+        query.setParameter(1, transformed);
+        return query.getResultList();
     }
 }
